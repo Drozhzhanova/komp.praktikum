37e51b0a1e783039807f207856812d6e38b983a1
Drozhzhanova <147833612+Drozhzhanova@users.noreply.github.com>
2024-06-17 22:08:26 +0300

    Update №2-метод итераций.cpp

--- Лабораторная работа 1/№2-метод итераций.cpp
+++ Лабораторная работа 1/№2-метод итераций.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+double g(double x) {
+    return 1 / exp(x) + 1;
+}
+
+double simpleIteration(double x0, double eps) {
+    double x = x0;
+    double next_x;
+    int iter = 0;
+
+    do {
+        next_x = g(x);
+        iter++;
+        x = next_x;
+    } while (std::abs(next_x - x) >= eps);
+
+    return x;
+}
+
+int main() {
+    double x0 = 2.0;
+    double eps = 1e-6;
+
+    double root = simpleIteration(x0, eps);
+
+    std::cout << "Root: " << root << std::endl;
+
+    return 0;
+}
 
