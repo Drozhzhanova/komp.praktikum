2f94a324e79d70579be08b2e2710b5c79b28e21d
Drozhzhanova <147833612+Drozhzhanova@users.noreply.github.com>
2024-06-17 22:12:59 +0300

    Update №2-метод итераций.py

--- Лабораторная работа 1/№2-метод итераций.py
+++ Лабораторная работа 1/№2-метод итераций.py
@@ -1,1 +1,15 @@
+def g(x):
+    return math.exp(-x)
 
+def fixed_point_iteration(x0, eps):
+    x1 = g(x0)
+    while abs(x1 - x0) > eps:
+        x0 = x1
+        x1 = g(x0)
+    return x1
+
+x0 = 1.5
+eps = 1e-6
+
+root = fixed_point_iteration(x0, eps)
+print(f"Root found at x = {root}")
