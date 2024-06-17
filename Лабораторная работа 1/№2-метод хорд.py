dd4876d1b7e953a20ab02582f1326fb789bd7214
Drozhzhanova <147833612+Drozhzhanova@users.noreply.github.com>
2024-06-17 22:10:15 +0300

    Update №2-метод хорд.py

--- Лабораторная работа 1/№2-метод хорд.py
+++ Лабораторная работа 1/№2-метод хорд.py
@@ -1,1 +1,23 @@
+def chord_method(a, b, eps):
+    if f(a) * f(b) > 0:
+        print("No root found in this interval")
+        return None
 
+    while abs(f(b)) > eps:
+        c = (a*f(b) - b*f(a)) / (f(b) - f(a))
+        if f(c) == 0.0:
+            return c
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+
+    return c
+
+a = 1.0
+b = 3.0
+eps = 1e-6
+
+root = chord_method(a, b, eps)
+if root:
+    print(f"Root found at x = {root}")
